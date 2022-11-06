1a5ad4b6424765abbcffb1582c8dc2e4048912c9
Валентин <astrotrain55@gmail.com>
2022-11-06 14:02:26 +0500

    refactor axios

--- templates/vue-template/src/api/Ajax.js
+++ templates/vue-template/src/api/Ajax.js
@@ -1,22 +1,22 @@
-import { serialize } from 'object-to-formdata';
+// import { serialize } from 'object-to-formdata';
+import qs from 'qs';
 import axios from 'axios';
 import config from '../config';
 
 const instance = axios.create({
   baseURL: config.ajaxUrl,
+  transformRequest: (data) => qs.stringify(data, { arrayFormat: 'brackets' }),
+  // transformRequest: (data, headers) => {
+  //   headers['Content-Type'] = 'multipart/form-data';
+  //   return serialize(data, {
+  //     indices: true,
+  //     nullsAsUndefineds: true,
+  //     booleansAsIntegers: true,
+  //     allowEmptyArrays: true,
+  //   });
+  // },
 });
 
 instance.interceptors.response.use(({ data }) => data);
 
-const Ajax = (...args) => Promise.all(args.map((req) => instance(req)));
-const serializeRequest = (data) => serialize(data, {
-  indices: true,
-  nullsAsUndefineds: true,
-  booleansAsIntegers: true,
-  allowEmptyArrays: true,
-});
-
-export {
-  Ajax,
-  serializeRequest,
-};
+export const Ajax = instance;
--- templates/vue-template/src/api/index.js
+++ templates/vue-template/src/api/index.js
@@ -1,15 +1,15 @@
-import { Ajax, serializeRequest } from './Ajax';
+import { Ajax } from './Ajax';
 
 export default {
   test(data = {}) {
     return Ajax({
       url: 'index.php',
       method: 'post',
-      params: serializeRequest(data),
-    }).then(this.handler.bind(this));
+      params: data,
+    }).then(this.handler);
   },
 
-  handler([response]) {
+  handler(response) {
     if (response.success) return response.data;
     throw new Error(response.message);
   },
