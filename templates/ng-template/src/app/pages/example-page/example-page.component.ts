de8446a3006eef6e900e22d6fcd2e5eee27ca077
Валентин <astrotrain55@gmail.com>
2024-05-23 18:22:10 +0500

    form-validation

--- templates/ng-template/src/app/pages/example-page/example-page.component.ts
+++ templates/ng-template/src/app/pages/example-page/example-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormValidationComponent } from '../../components/form-validation/form-validation.component';
 
 @Component({
   selector: 'app-example-page',
   standalone: true,
-  imports: [],
+  imports: [FormValidationComponent],
   templateUrl: './example-page.component.html',
   styleUrl: './example-page.component.scss',
 })
